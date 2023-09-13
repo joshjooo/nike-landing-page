cf528acd4075c134ecc0805ae2c4084d47f86c5c
Joshua James <joshuagjames@gmail.com>
2023-09-13 18:21:56 +0300

    first commit

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,26 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
+
+
+const CustomerReviews = () => {
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold ">
+        What Our 
+        <span className="text-coral-red"> Customer </span>
+        Say
+      </h3>
+      <p className=" info-text m-auto mt-4 max-w-lg text-center ">
+        Here genuine stories from our customer about their exceptional expirience with us</p>
+      <div className="mt-24 flex flex-1 justify-evenly 
+      items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} {...review} />
+        ))}
+
+      </div>
+    </section>
+  )
+}
+
+export default CustomerReviews
